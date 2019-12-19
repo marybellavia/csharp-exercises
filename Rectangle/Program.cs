5d04b476c244bf05588b8e2991a3fd21315cc939
Mary Bellavia <sauerwein.mary@gmail.com>
2019-12-19 10:11:58 -0600

    rectangle

---
+++ Rectangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int area = width * height;
+
+            Console.WriteLine("The area of the rectangle was " + area);
+        }
+    }
+}
